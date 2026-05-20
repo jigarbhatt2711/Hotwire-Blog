15264c456b33cf9ff914e9e8258f96ac67625a6a
jigar bhatt <jigar@iservegroup.com>
2026-05-20 16:53:21 +0530

    Basic blogs CRUD created with seed test data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,4 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+# Create sample posts
+Post.create(title: "First Post", content: "Hello Hotwire!", author: "Jigar")
+Post.create(title: "Rails 8 is Amazing", content: "Let us learn Hotwire!", author: "Jigar")
+Post.create(title: "Turbo Streams", content: "Real-time without JavaScript!", author: "Jigar")
