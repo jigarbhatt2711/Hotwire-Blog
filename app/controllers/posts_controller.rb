f23caad6d50139abe388132f8606b995fb98682f
jigar bhatt <jigar@iservegroup.com>
2026-05-22 15:58:06 +0530

    Turbo-Stream implemented on Destroy action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -60,6 +60,7 @@ class PostsController < ApplicationController
     @post.destroy!
 
     respond_to do |format|
+      format.turbo_stream
       format.html { redirect_to posts_path, notice: "Post was successfully destroyed.", status: :see_other }
       format.json { head :no_content }
     end
